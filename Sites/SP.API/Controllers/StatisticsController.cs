27bbe90f36f82663476656bc8ce70122660aabf3
Gideon Bakx <gideon@bakx.ca>
2020-02-23 20:31:37 -0500

    Added the statistics

--- Sites/SP.API/Controllers/StatisticsController.cs
+++ Sites/SP.API/Controllers/StatisticsController.cs
@@ -1,8 +1,10 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SP.Models;
+using SP.Models.Statistics;
 
 namespace SP.API.Controllers
 {
@@ -15,6 +17,81 @@ namespace SP.API.Controllers
         public StatisticsController(ILogger<StatisticsController> log)
         {
             this.log = log;
+        }
+
+        [HttpGet]
+        [Route(nameof(GetTopCountries))]
+        [ResponseCache(Duration = 300, Location = ResponseCacheLocation.Any, NoStore = false)]
+        public async Task<IEnumerable<TopCountries>> GetTopCountries(int top = 10)
+        {
+            log.LogDebug(
+                $"Received call from {Request.HttpContext.Connection.RemoteIpAddress} to {nameof(GetTopCountries)}.");
+
+            // Open handle to database
+            await using Db db = new Db();
+
+            // Return object
+            return db.StatisticsBlocks
+                .AsEnumerable()
+                .GroupBy(s => s.Country)
+                .Select(cl => new TopCountries
+                {
+                    Country = cl.First().Country,
+                    Attempts = cl.Sum(s => s.Attempts),
+                })
+                .OrderByDescending(s => s.Attempts)
+                .Take(top)
+                .ToList();
+        }
+
+        [HttpGet]
+        [Route(nameof(GetTopCities))]
+        [ResponseCache(Duration = 300, Location = ResponseCacheLocation.Any, NoStore = false)]
+        public async Task<IEnumerable<TopCities>> GetTopCities(int top = 10)
+        {
+            log.LogDebug(
+                $"Received call from {Request.HttpContext.Connection.RemoteIpAddress} to {nameof(GetTopCities)}.");
+
+            // Open handle to database
+            await using Db db = new Db();
+
+            // Return object
+            return db.StatisticsBlocks
+                .AsEnumerable()
+                .GroupBy(s => s.City)
+                .Select(cl => new TopCities
+                {
+                    City = cl.First().City.Length > 0 ? cl.First().City : "Unknown",
+                    Attempts = cl.Sum(s => s.Attempts),
+                })
+                .OrderByDescending(s => s.Attempts)
+                .Take(top)
+                .ToList();
+        }
+
+        [HttpGet]
+        [Route(nameof(GetTopIps))]
+        [ResponseCache(Duration = 300, Location = ResponseCacheLocation.Any, NoStore = false)]
+        public async Task<IEnumerable<TopIps>> GetTopIps(int top = 10)
+        {
+            log.LogDebug(
+                $"Received call from {Request.HttpContext.Connection.RemoteIpAddress} to {nameof(GetTopCities)}.");
+
+            // Open handle to database
+            await using Db db = new Db();
+
+            //
+            return db.LoginAttempts
+                .AsEnumerable()
+                .GroupBy(s => s.IpAddress)
+                .Select(cl => new TopIps
+                {
+                    IpAddress = cl.First().IpAddress,
+                    Attempts = cl.Count()
+                })
+                .OrderByDescending(s => s.Attempts)
+                .Take(10)
+                .ToList();
         }
 
         [HttpPost]
---
+++ Sites/SP.Overview/wwwroot/js/statistics.js
@@ -0,0 +1,114 @@
+﻿"use strict";
+var baseUrl = "http://local.api.sp.bakx.ca";
+
+window.chartColors = [];
+
+window.onload = function () {
+
+    // Create colors
+    window.chartColors.push({ name: "Banana Yellow", color: "rgb(255, 236, 33)" });
+    window.chartColors.push({ name: "Brilliant Azure", color: "rgb(55, 138, 255)" });
+    window.chartColors.push({ name: "Deep Saffron", color: "rgb(255, 163, 47)" });
+    window.chartColors.push({ name: "Tart Orange", color: "rgb(245, 79, 82)" });
+    window.chartColors.push({ name: "Kiwi", color: "rgb(147, 240, 59)" });
+    window.chartColors.push({ name: "Lavender Indigo", color: "rgb(149, 82, 234)" });
+    window.chartColors.push({ name: "Chinese Orange", color: "rgb(246, 109, 68)" });
+    window.chartColors.push({ name: "Rajah", color: "rgb(254, 174, 101)" });
+    window.chartColors.push({ name: "Key Lime", color: "rgb(230, 246, 157)" });
+    window.chartColors.push({ name: "Light Moss Green", color: "rgb(170, 222, 167)" });
+    window.chartColors.push({ name: "Green Sheen", color: "rgb(100, 194, 166)" });
+    window.chartColors.push({ name: "Cyan Cornflower Blue", color: "rgb(45, 135, 187)" });
+    window.chartColors.push({ name: "Princeton Orange", color: "rgb(244, 122, 31)" });
+    window.chartColors.push({ name: "Saffron", color: "rgb(253, 187, 47)" });
+    window.chartColors.push({ name: "Japanese Laurel", color: "rgb(55, 123, 43)" });
+    window.chartColors.push({ name: "Apple", color: "rgb(122, 193, 66)" });
+    window.chartColors.push({ name: "Ocean Boat Blue", color: "rgb(0, 124, 195)" });
+    window.chartColors.push({ name: "USAFA Blue", color: "rgb(0, 82, 155)" });
+
+    // window.chartColors.push( { name: 'a', color: 'rgb'});
+
+    // Load statistics
+    loadStatistics();
+};
+
+
+function loadStatistics() {
+    getData("countriesChart", "Top 10 countries", baseUrl + "/statistics/GetTopCountries", "country", createChart);
+    getData("citiesChart", "Top 10 cities", baseUrl + "/statistics/GetTopCities", "city", createChart);
+    getData("ipChart", "Top 10 ip", baseUrl + "/statistics/GetTopIps", "ipAddress", createChart);
+}
+
+function createChart(elem, title, data, dataElement) {
+
+    var config = {
+        type: "doughnut",
+        data: {
+            datasets: [],
+            labels: []
+        },
+        options: {
+            responsive: true,
+            legend: {
+                position: "bottom"
+            },
+            title: {
+                display: true,
+                text: title
+            },
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            }
+        }
+    };
+
+    var ctx = document.getElementById(elem).getContext("2d");
+    window.myDoughnut = new Chart(ctx, config);
+
+    // Create empty dataset
+    var dataset = {
+        data: [],
+        backgroundColor: []
+    }
+
+    for (let i = 0; i < data.length; i++) {
+
+        // Populate data
+        dataset.data.push(data[i].attempts);
+
+        // Add color
+        dataset.backgroundColor.push(getColor(i).color);
+
+        // Add label
+        config.data.labels.push(data[i][dataElement]);
+    }
+
+    // Add dataset
+    config.data.datasets.push(dataset);
+
+    // Update pie chart
+    window.myDoughnut.update();
+}
+
+function getData(elem, title, url, dataElement, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            callback(elem, title, JSON.parse(xhr.responseText), dataElement);
+            return;
+        }
+
+        alert("Request failed.  Returned status of " + xhr.status);
+    };
+    xhr.send();
+}
+
+function getColor(i) {
+    if (i > window.chartColors.length) {
+        console.warn("getColor() out of bounds");
+        i = 0;
+    }
+
+    return window.chartColors[i];
+}
